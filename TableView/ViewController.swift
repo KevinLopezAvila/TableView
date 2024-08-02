afe993d954a5c08c12d5ca0cd67ab0b6e1867154
KevinLopezAvila <keflopavi2001@gmail.com>
2024-08-02 09:38:11 -0600

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by KevinLA on 02/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
